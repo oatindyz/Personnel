f742f58f200f48bef770afc1262142f3f8bc1046
Cheezzie O_O <zplaygiirlz1@hotmail.com>
2016-08-31 19:05:48 +0700

    ASD

---
+++ Personnel/Connection/ConnectionDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Web;
+
+namespace Rmutto.Connection
+{
+    public static class ConnectionDB
+    {
+        private static OracleConnection conn;
+        static ConnectionDB()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+            string connectionString = ConfigurationManager.ConnectionStrings["ORCL_RMUTTO"].ToString();
+            conn = new OracleConnection(connectionString);
+        }
+
+        public static OracleConnection GetOracleConnection()
+        {
+            return conn;
+        }
+
+
+    }
+}
---
+++ Personnel/Script/JSFunction.js
@@ -0,0 +1,47 @@
+﻿function allowOnlyNumber(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+function maxBuy(max, evt) {
+    var m = parseInt(max);
+    var input = parseInt(evt.value);
+
+    if (input > max) {
+        alert("ไม่สามารถระบุจำนวนมากกว่า : " + max);
+        evt.value = max;
+    }
+function reFocus(id) {
+        document.getElementById(id).blur();
+        document.getElementById(id).focus();
+    }
+
+}
+function openPopup(a) {
+    document.getElementById(a).style.display = 'block';
+}
+function closePopup(a) {
+    document.getElementById(a).style.display = 'none';
+}
+function OnClick(evt) {
+    var clickCount;
+
+    if (clickCount >= 2) {
+        clickCount = 1;          //reset clickCount
+        SingleClickFunction();
+        return false;
+    }
+}
+function AllowClickOnce(linkbuttonID) {
+    var linkbutton = document.getElementById(linkbuttonID);
+    if (linkbutton.clicked == '0') {
+        linkbutton.clicked = '1';
+        linkbutton.disabled = true;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
